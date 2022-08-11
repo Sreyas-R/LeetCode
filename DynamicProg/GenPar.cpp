a50c6cc0a44aa72fdc2ac508d13e3188f3deef83
Sreyas-R <sreyasrejil1@gmail.com>
2022-08-12 01:04:29 +0530

    Generate Parenthesis

---
+++ DynamicProg/GenPar.cpp
@@ -0,0 +1,38 @@
+
+//https://leetcode.com/problems/generate-parentheses/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<string> dp(int n, int m , vector<string> &ans , string brackets)
+{
+    if(m==0 && n==0)
+    {
+        ans.push_back(brackets);
+        return ans;
+    }
+
+    if(n>0) //Left bracket is not zero then add a left bracket recursively
+        dp(n-1,m+1,ans,brackets+"(");
+    if(m>0)
+        dp(n,m-1,ans,brackets+")");
+
+
+
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<string> ans;
+    
+    dp(n,0,ans,"");
+
+    for(auto i : ans)
+        cout<<i<<" ";
+        
+    
+}
+
